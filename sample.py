10d6b3ffdc8311a0d8c1577df3db853f56e419ec
punarvgupta <punarv.academics@gmail.com>
2026-06-02 13:08:19 +0530

    rebottttttt

--- sample.py
+++ sample.py
@@ -1,2 +1,3 @@
 # this is my first code in github session 
-print("Punarv gupta coding session")
+print("Punarv gupta coding session")\
+    print("this is my first session of coding in vs studio code and github")
