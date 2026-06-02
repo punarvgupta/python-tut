8755653c0beda6fd0cc1ad970a38d8a86eee6c22
punarvgupta <punarv.academics@gmail.com>
2026-06-02 14:51:21 +0530

    Day 1 Learning

--- sample.py
+++ sample.py
@@ -1,3 +1,69 @@
-# this is my first code in github session 
-print("Punarv gupta coding session")\
-    print("this is my first session of coding in vs studio code and github")
+#Machine scan only understand combination of 0 and 1 .
+#Python is and an simple language to understand and it was developed by Guido van Rossum and it is portable.
+name="Punarv Gupta"
+age=-12
+price=25.99 
+print(type(name))
+print(type(age))
+print(type(price))
+
+# We have five major types of data types in python which are string, integer, float, boolean and none
+
+#We also major three types of string which are single quote (' '), double quote (" ") and triple quote (''' ''').
+# Boolean is used to represent the value of true and false and it is denoted by True and False in python.
+# Example boolean and none :-
+
+age=12
+old=False
+a=None
+print(type(age))
+print(type(old))
+print(type(a))
+
+# Python is a case - sensitive language which means that it treats uppercase and lowercase letters differently.
+
+a=1000
+b=500
+sum=a-b
+print(sum) 
+
+# arthemetic expression 
+
+a=5
+b=2
+sum=a+b
+print(sum) 
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+# relational operators or compare operators
+
+a=50
+b=20
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#types converstion
+a="2"
+b=4.25
+sum=a+b
+print(sum)
+
+# We can not add string and float together because they are different data types. We need to convert the string to a float or the float to a string before we can add them together. so we can use the following instead.
+
+a=int("2")
+b=float("4.25")
+sum=a+b
+print(sum)
+
+# For better explination of the code see below
+
+print(type(a))
+print(a + b)
