da97c691fed5d481c5cf675ae9ac1198c5e43dbb
punarvgupta <punarv.academics@gmail.com>
2026-06-02 15:26:17 +0530

    practice

--- day1/day1_practise.py
+++ day1/day1_practise.py
@@ -1,1 +1,25 @@
-#bhfvy
+# Q1. write a Program to input 2 numbers & print their sum.
+
+'''var1=int(input("Enter your first number : "))
+var2=int(input("Enter your second number : "))
+sum=var1+var2
+print("The sum of the two numbers given by you are : ", sum)'''
+
+# Q2. write a proram to input the side of a square & print it's area.
+
+'''var1=int(input("enter the side of square in centimeters : "))
+area=var1*var1
+print("The area of the square is : ", area, "centimeter square.")'''
+
+# Q3. write a program to input 2 floating point number and tell their average.
+
+var1=float(input("enter your first number in decimals : " ))
+var2=float(input("enter your second number in decimals : " ))
+var3=(var1+var2)/2
+print ("The average of the two number given by you is : ", var3)
+
+# Q4. write a programe to input to integer numbers , a and b. Print true if a is greater than or equal to b. If not then print false.
+
+a=int(input("enter your first number : "))
+b=int(input("enter your second number : "))
+print(a>=b)
