5f5ddb5b235d1846acbf0817df389a33bb2eb5d4
punarvgupta <punarv.academics@gmail.com>
2026-06-04 14:54:21 +0530

    never

--- day2/day2_learning.py
+++ day2/day2_learning.py
@@ -1,2 +1,8 @@
+# Day 2, string and conditional statements
+# String
 
-lol
+#String is a data type that stores sequence of characters.
+
+#Escape sequence characters are those characters that inserts 2 spaces in one string.
+# '\n' is used to insert a new line in a string.
+# '\t' is used to insert a tab in a string.
