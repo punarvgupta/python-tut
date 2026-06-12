bdb3ab1e71d56adf0a09bdce754a5d3d2a5242b4
punarvgupta <punarv.academics@gmail.com>
2026-06-12 16:54:59 +0530

    practice

---
+++ day5/day5_practice.py
@@ -0,0 +1,42 @@
+#  Q1. write a program to print numbers from 1 to 100 .
+
+'''i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+# Q2. Write a program to print numbers from 100 to 1 .
+
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+
+# Q3. print the multiplication table of number n .
+
+i = 1 
+while i <= 10:
+    print(3*i)
+    i += 1
+    
+# Q4. print the elements of the following list using a loop . 
+
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+nums=[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+idx=0
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1'''
+    
+# Search for a number x in this tuple using loop .
+
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+i = 0
+while i < len(nums):
+    print(nums[i])
+    i += 1
