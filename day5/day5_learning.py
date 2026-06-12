b276bc1f8faf156c3cb0d30ba58d67732e3333b8
punarvgupta <punarv.academics@gmail.com>
2026-06-12 16:14:42 +0530

    learning

--- day5/day5_learning.py
+++ day5/day5_learning.py
@@ -1,1 +1,21 @@
-"LOL"
+# LOOPS
+
+# Loops are used to repeat instructions.
+
+# There are two main types of loops called while and for .
+# while loops
+
+# while loops are loops that keep running until a specific count
+# for example :
+
+count = 1
+while count <= 5 :
+    print("hello")
+    count += 1
+
+# the following code was to print hello again and again till it printed 5 times .
+print(7)
+i = 1
+while i <= 5:
+    print("hello")
+    i += 1 
